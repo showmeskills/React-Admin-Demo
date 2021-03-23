586c364c26d9547c231efb89447bd00fd8997434
Terrance <465446853yan@gmail.com>
2021-03-23 20:34:59 +1100

    touch a config-overrides file that is related with customize-car, react-app-rewired and less/less-loader

---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+/**
+ * @file config-overrides.js
+ * @author TerryYan (465446853yan@gmail.com)
+ * the configuration of the file is based on customize-cra and react-app-rewired 
+ */
+
+//import relative methods
+const {
+    override,
+    addDecoratorsLegacy,
+    fixBabelImports,
+    addLessLoader
+} = require('customize-cra')
+
+const theme = require('./theme')
+module.exports = override(
+    addDecoratorsLegacy(),
+    fixBabelImports('import',{
+      library:'antd',
+      libraryDirectory:'es',
+      style:true,
+    }),
+    addLessLoader({
+      modifyVars:theme,
+      javascriptEnabled:true,
+    }),
+);
