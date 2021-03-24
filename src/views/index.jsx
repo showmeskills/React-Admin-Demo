f97698c48a4229d7c09957bb3c99ce9d17f9ac0d
Terrance <465446853yan@gmail.com>
2021-03-24 15:38:09 +1100

    adding a lazy function in router

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,6 +1,42 @@
-export {default as NotFound} from './NotFound'
-export {default as Login} from './Login'
-export {default as Settings} from './Settings'
-export {default as Dashboard} from './Dashboard'
-export {default as ArticleList} from './Article'
-export {default as ArticleEdit} from './Article/Edit'
+//lazy loading
+import Loadable from 'react-loadable';
+import {Loading} from '../components'
+
+
+const Dashboard = Loadable ({
+    loader:()=>import('./Dashboard'),
+    loading:Loading
+})
+const Login = Loadable ({
+    loader:()=>import('./Login'),
+    loading:Loading
+})
+const NotFound = Loadable ({
+    loader:()=>import('./NotFound'),
+    loading:Loading
+})
+const Settings = Loadable ({
+    loader:()=>import('./Settings'),
+    loading:Loading
+})
+const ArticleList = Loadable ({
+    loader:()=>import('./Article'),
+    loading:Loading
+})
+const ArticleEdit = Loadable ({
+    loader:()=>import('./Article/Edit'),
+    loading:Loading
+})
+
+
+
+
+export {
+    Login,
+    NotFound,
+    Settings,
+    Dashboard,
+    ArticleList,
+    ArticleEdit
+}
+
